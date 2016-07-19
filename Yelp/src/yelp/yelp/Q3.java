fbc8d78c8ab1aefc9ead8238794afeb1a76ec433
Anoop S Somashekar <anoopsskumar@gmail.com>
2016-07-19 13:53:42 -0500

    Update Q3.java

--- Yelp/src/yelp/yelp/Q3.java
+++ Yelp/src/yelp/yelp/Q3.java
@@ -1,4 +1,4 @@
-package yelpjoin.yelpjoin;
+package yelp.yelp;
 
 import java.io.IOException;
 import java.util.regex.Matcher;
@@ -126,6 +126,6 @@ public class Q3
 		
 		//Wait till job completion
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		//hadoop jar YelpJoin-0.0.1-SNAPSHOT.jar yelpjoin.yelpjoin.Q3 /user/ass150430/Yelp_Q3 /user/ass150430/Q3_OP
+		//hadoop jar Yelp-0.0.1-SNAPSHOT.jar yelp.yelp.Q3 /user/ass150430/Yelp_Q3 /user/ass150430/Q3_OP
 	}
 }
