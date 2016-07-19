0fe1f957d6945ada30fc7a74fd011988d73c419a
Anoop S Somashekar <anoopsskumar@gmail.com>
2016-07-19 13:54:09 -0500

    Update Q4.java

--- Yelp/src/yelp/yelp/Q4.java
+++ Yelp/src/yelp/yelp/Q4.java
@@ -1,4 +1,4 @@
-package yelpjoin.yelpjoin;
+package yelp.yelp;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -182,6 +182,6 @@ public class Q4
 		
 		//Wait till job completion
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		//hadoop jar YelpJoin-0.0.1-SNAPSHOT.jar yelpjoin.yelpjoin.Q4 /user/ass150430/Yelp_Q4 /user/ass150430/Q4_OP
+		//hadoop jar Yelp-0.0.1-SNAPSHOT.jar yelp.yelp.Q4 /user/ass150430/Yelp_Q4 /user/ass150430/Q4_OP
 	}	
 }
